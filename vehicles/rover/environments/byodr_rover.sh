046ec99f5810b0ade984f9f4a129bf81515f625a
Esjay Smith <esjay.smith@gmx.com>
2020-02-17 12:49:27 +0100

    mv env variables to scripts

--- vehicles/rover/environments/byodr_rover.sh
+++ vehicles/rover/environments/byodr_rover.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DC_JSON_CONFIG=profile-rover.json
-DC_JSON_ROVER=profile-rover.json
-DC_INFERENCE_MODELS=/mnt/data1/R3/Models/Xa1
-DC_PILOT_HZ=50
-DC_VEHICLE_HZ=50
-DC_INFERENCE_HZ=10
-DC_INFERENCE_GPU=0
-DC_RECORDER_HZ=12
-DC_RECORDER_SESSIONS=/mnt/data1/R3/sessions
-DC_TELEOP_PATIENCE=0.250
-DC_ROS_SERIAL_DEVICE=/dev/ttyACM0
+export DC_JSON_CONFIG=profile-rover.json
+export DC_JSON_ROVER=profile-rover.json
+export DC_INFERENCE_MODELS=/mnt/data1/R3/Models/Xa1
+export DC_PILOT_HZ=50
+export DC_VEHICLE_HZ=50
+export DC_INFERENCE_HZ=10
+export DC_INFERENCE_GPU=0
+export DC_RECORDER_HZ=12
+export DC_RECORDER_SESSIONS=/mnt/data1/R3/sessions
+export DC_TELEOP_PATIENCE=0.250
+export DC_ROS_SERIAL_DEVICE=/dev/ttyACM0
