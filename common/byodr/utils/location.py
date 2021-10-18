802ef6ae140bcccbcdfc3bb766163736239500cb
Esjay Smith <esjay.smith@gmx.com>
2021-10-18 14:49:36 +0200

    cache geodesic calculations on the position arguments

--- common/byodr/utils/location.py
+++ common/byodr/utils/location.py
@@ -1,5 +1,6 @@
 import collections
 
+import cachetools
 from geographiclib.geodesic import Geodesic
 
 
@@ -14,14 +15,12 @@ def path(current_position, previous_position):
 
 
 class GeoTracker(object):
-    def __init__(self, min_distance_meters=0.05):
+    def __init__(self, cache_ttl=10.0, min_distance_meters=0.05):
         self._min_distance = min_distance_meters
         self._positions = collections.deque(maxlen=2)
+        self._cache = cachetools.TTLCache(maxsize=100, ttl=cache_ttl)
 
-    def clear(self):
-        self._positions.clear()
-
-    def track(self, position):
+    def _track(self, position):
         n_positions = len(self._positions)
         if n_positions == 0:
             if position is None:
@@ -51,3 +50,14 @@ class GeoTracker(object):
             else:
                 distance, bearing = path((position[0], position[1]), self._positions[-2])
                 return position[0], position[1], bearing
+
+    def clear(self):
+        self._positions.clear()
+
+    def track(self, position):
+        _key = position
+        res = self._cache.get(_key)
+        if res is None:
+            res = self._track(position)
+            self._cache[_key] = res
+        return res
