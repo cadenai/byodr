b398e6425e53a3764ff7410c2970896fb1fcafa5
Esjay Smith <esjay.smith@gmx.com>
2022-03-07 09:46:48 +0100

    httpd; in support of automatic service software update

--- httpd/wrap.py
+++ httpd/wrap.py
@@ -12,15 +12,33 @@ logger = logging.getLogger(__name__)
 log_format = '%(levelname)s: %(asctime)s %(filename)s %(funcName)s %(message)s'
 
 
+def _check_config(config_file):
+    with open(config_file, 'r') as _file:
+        contents = _file.read()
+    if 'backend pilot' in contents:
+        logger.info("The proxy configuration is up to date.")
+    else:
+        # Not all routers are at the default ip.
+        _ip_id = contents.index(':9101')
+        _ip = contents[_ip_id - 12: _ip_id]
+        _ssl = ('ssl crt' in contents)
+        with open('haproxy_ssl.template' if _ssl else 'haproxy.template', 'r') as _template:
+            with open(config_file, 'w') as _file:
+                _file.write(_template.read().replace('192.168.1.32', _ip))
+        logger.info("Updated the existing proxy configuration using ip '{}'.".format(_ip))
+
+
 def main():
     parser = argparse.ArgumentParser(description='Http proxy server.')
     parser.add_argument('--config', type=str, default='/config/haproxy.conf', help='Configuration file.')
     args = parser.parse_args()
 
     config_file = args.config
-    if not os.path.exists(config_file):
+    if os.path.exists(config_file):
+        _check_config(config_file)
+    else:
         shutil.copyfile('haproxy.template', config_file)
-        logger.info("Created a new proxy configuration file from template.")
+        logger.info("Created a new non ssl proxy configuration.")
     subprocess.call(['/usr/sbin/haproxy', '-f', config_file])
 
 
