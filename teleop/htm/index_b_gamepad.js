01f7e18833f7153aab069ea20d7e24a75465d89f
Esjay Smith <esjay.smith@gmx.com>
2022-04-27 12:32:15 +0200

    teleop ui gamepad; trust de 'standard' gamepad mapping of the browser

--- teleop/htm/index_b_gamepad.js
+++ teleop/htm/index_b_gamepad.js
@@ -112,7 +112,7 @@ var gamepad_controller = {
         var result = null;
         if (gamepad.mapping == 'standard' && gid.includes('45e')) {
             result = Object.create(Xbox360StandardController);
-        } else if (gamepad.mapping == 'standard' && gid.includes('54c')) {
+        } else if (gamepad.mapping == 'standard') {
             result = Object.create(PS4StandardController);
         //
         // Not to be used yet - without standard mapping the triggers can result in non-zero throttle before use.
